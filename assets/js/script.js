a302603af115404b157348b64bb9af3dd5334200
Amnasaqib <>
2023-11-09 21:02:06 +0000

    Adding variables to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// global variables
+
+const heading = document.getElementById("heading");
+const menu = document.getElementById("menu");
+const rulesSection = document.getElementById("rules");
+const quizArea = document.getElementById("quiz-area");
+const answerArea = document.getElementById("answer-area");
+const timeDisplay = document.getElementById("timer");
+const nextButton = document.getElementById("next-button");
