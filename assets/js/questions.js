aae4315a368998ee1d2819a9572c1cb38362c3e1
Amnasaqib <>
2023-11-09 20:54:12 +0000

    Adding more questions and answers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -53,64 +53,96 @@ answer: [
  {
 question: "Which player has hit more sixes?",
 answer: [
-
-    
+    { text: "Virat Kohli", correct: false },
+    { text: "Rohit Sharma", correct: true },
+    { text: "Ben Stokes", correct: false },
+    { text: "Liam Livingstone", correct: false}
 ]
-          }
+          },
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     {
+question: "Which team has won the most World cups?",
+answer: [
+    { text: "Australia", correct: true },
+    { text: "India", correct: false },
+    { text: "England", correct : false },
+    { text: "South-Africa", correct : false}
 ]
+     } ,    
+
+{
+question: "Which player has most centuries?"
+answer: [
+    { text: "Ricky Ponting", correct: false },
+    { text: "Kumar Sangakkara", correct: false },
+    { text: "Sachin Tendulkar", correct: true },
+    { text: "Eoin Morgan", correct: false }
+]
+},
+ {
+question: "What is the highest team score in ODI?",
+answer: [
+{ text: "450-0", correct: false },
+{ text: "498-4", correct: true },
+{ text: "450-4", correct : false },
+{ text: "498-0", correct: false } 
+]
+ },
+ {
+question: "What is the highest team score in T20i?",
+answer: [
+    { text: "278-3", correct: false },
+    { text: "278-4", correct: false },
+    { text: "273-2", correct: false },
+    { text: "314-3", correct: true } 
+]
+}, 
+ 
+{
+ question: "Which batsman has best strike rate in ODI cricket?",
+ answer: [
+    { text: "Andrew Russell", correct: true },
+    { text: "Glenn Maxwell", correct: false },
+    { text: "Jos Buttler", correct: false },
+    { text: "Shahid Afridi", correct: false}
+ ]
+}, 
+{ 
+    question: "How many days does a Test match go for?",
+    answer: [
+        { text: "3", correct: false },
+        { text: "4", correct: false },
+        { text: "1", correct: false },
+        { text: "5", correct: true}
+    ]
+},
+ {
+    question:"When was first T20 match played?",
+    answer: [
+        { text: "2007", correct: false },
+        { text: "2000", correct: false },
+        { text: "2003", correct: true },
+        { text: "1999", correct: false}
+    ]
+ },
+
+ {
+question: "Which country is hosting 2023 World cup?"
+answer: [
+    { text: "India", correct: true },
+    { text: "England", correct: false},
+    { text: "New Zealand", correct: false},
+    { text: "Afghanistan", correct: false}
+]
+ },
+  {
+question: "What is the biggest Cricket rivalry?",
+answer: [
+    { text: "England Vs Austrailia", correct: false },
+    { text: "India Vs Pakistan", correct: true },
+    { text: "Bangladesh Vs Sri-Lanka", correct: false },
+    { text: "New Zealand Vs Australia", correct: false }
+]
+  },
+];
 
