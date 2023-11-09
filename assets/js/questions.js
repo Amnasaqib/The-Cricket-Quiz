192e1ad4901b7c39799b72241d044fca92707646
Amnasaqib <>
2023-11-09 19:58:03 +0000

    Adding questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,116 @@
+// object containing questions.
+
+const Questions = [
+    {
+question: "Who is the top scorer of all time?",
+answers: [
+    { text: "MS Dhoni", correct: false },
+    { text: "Sachin Tendulkar", correct: true },
+    { text: "Graeme Smith", correct: false },
+    { text: "Joe Root", correct: false }
+]
+    },
+
+{
+question: "Who is top wicket taker of all time?",
+answer: [
+{ text: "Muthiah Muralidaran", correct: true },
+{ text: "Wasim Akram", correct: false },
+{ text: "James Anderson", correct: false },
+{ text: "Daniel Vettori", correct: false }
+]
+   },
+    
+{
+question: "How many players are there in a cricket team?",
+answer: [
+{ text: "12", correct: false },
+{ text: "9",  correct: false},
+{ text: "10", correct: false },
+{ text: "11", correct: true }
+]
+  },
+ {
+ question: "What is the biggest cricket tournament?",
+ answer: [
+{ text: "Ashes", correct: false },
+{ text: "World Cup", correct: true },
+{ text: "Champions Trophy", correct: false },
+{ text: "Series", correct: false }
+ ]
+    },
+
+    { 
+    question: "Which Pakistani player has scored 200?",
+    answer: [
+        { text: "Babar Azam", correct: false },
+        { text: "Saeed Anwar", correct: false },
+        { text: "Fakhar Zaman", correct: true },
+        { text: "Younis Khan", correct: false}
+    ]
+  },
+
+ {
+question: "Which player has hit more sixes?",
+answer: [
+
+    
+]
+          }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
