c32718a9829d8fa8722261d66bc0999d4c7ecb4f
Amnasaqib <>
2023-11-09 21:07:04 +0000

    Writing down the variables to be defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@ const quizArea = document.getElementById("quiz-area");
 const answerArea = document.getElementById("answer-area");
 const timeDisplay = document.getElementById("timer");
 const nextButton = document.getElementById("next-button");
+
+// variables to be defined 
+ let shuffledQuestion;
+ let currentQuestionIndex;
+ let correctAnswers;
+ let score;
+ let timeLeft;
+ let timerInterval;
+
+ 
